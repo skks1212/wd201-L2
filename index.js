c541290376dc4e931e6411f17478af75d0b0898a
Shivank Kacker <kacker.shivank@gmail.com>
2022-10-12 21:00:29 +0530

    l2

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-const hello = () => {
-    console.log("Hello Node.Js!");
-}
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv.slice(2));
 
-hello();
+const port = parseInt(args.port) || 3000;
+
+let homeContent = "";
+let projectContent = "";
+let regContent = "";
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
+});
+
+fs.readFile("registration.html", (err, reg) => {
+    if (err) {
+        throw err;
+    }
+    regContent = reg;
+});
+
+http
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/registration":
+                response.write(regContent);
+                response.end();
+                break;
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    })
+    .listen(port);
